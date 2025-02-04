6aa9a8c82befef7ff11c50773ddd431f0e760ba5
5tellar5tar <skold2@outlook.com>
2025-02-04 15:17:33 +0100

    added js to close menu when selecting

---
+++ js/app.js
@@ -0,0 +1,12 @@
+window.onload="test()";
+
+
+function test(){
+    const radioMenu1 = document.getElementById("radioMenu1");
+    const radioMenu2 = document.getElementById("radioMenu2");
+    const radioMenu3 = document.getElementById("radioMenu3");
+    const checkboxMenu = document.getElementById("checkboxMenu")
+}
+function unCheck(){
+    checkboxMenu.checked = false;
+}
